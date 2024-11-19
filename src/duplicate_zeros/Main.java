eb0bf4a5adc4db44269b5b8785cd2d2fa25e85d5
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-19 16:59:33 +0500

    dupliate zeros

---
+++ src/duplicate_zeros/Main.java
@@ -0,0 +1,23 @@
+package duplicate_zeros;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {1,0,2,3,0,4,5,0};
+        duplicateZeros(arr);
+    }
+
+    public static void duplicateZeros(int[] arr) {
+        int [] newArray = arr.clone();
+        int n = arr.length;
+        int j=0;
+        for(int i = 0; i<n && j<n; i++ ){
+            arr[j++] = newArray[i];
+            if(newArray[i]==0 && j<n){
+                arr[j++]=0;
+            }
+        }
+
+    }
+}
