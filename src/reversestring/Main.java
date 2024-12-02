b907e5469cef52b7adc935ce657cfbfb29a0e1c2
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-12-02 16:46:50 +0500

    reverse string added

---
+++ src/reversestring/Main.java
@@ -0,0 +1,21 @@
+package reversestring;
+
+public class Main {
+    public static void main(String[] args) {
+      char []  s = {'h','e','l','l','o'};
+      reverseString(s);
+
+    }
+
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while(right>left){
+            char temp = s[left];
+            s[left++]=s[right];
+            s[right--]=temp;
+        }
+
+        System.out.println(s);
+    }
+}
