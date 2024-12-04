a547d6913169698653e4dc70d6b9c1e4b405cafc
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-12-04 18:16:30 +0500

    convert Temperature

---
+++ src/converTemperature/Main.java
@@ -0,0 +1,18 @@
+package converTemperature;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(convertTemperature(16.9)));
+    }
+
+    public static double[] convertTemperature(double celsius) {
+        double[] arr=new double[2];
+        arr[1]=1.80*celsius+32.00;
+        arr[0]=celsius+273.15;
+        return arr;
+    }
+}
