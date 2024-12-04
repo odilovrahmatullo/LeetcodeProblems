cd58e428059314831ce51d70381ed9d59292a56c
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-12-04 18:16:41 +0500

    reverse vowel

---
+++ src/reversevowel/Main.java
@@ -0,0 +1,45 @@
+package reversevowel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "IceCreAm";
+        System.out.println(reverseVowels(s));
+    }
+
+
+    public static String reverseVowels(String s) {
+        boolean[] vowels = new boolean[128];
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            vowels[c] = true;
+        }
+        char[] c = s.toCharArray();
+        int left = 0;
+        int right = c.length - 1;
+        while (right > left) {
+            if(!vowels[c[left]]){
+                left++;
+            }
+            if(!vowels[c[right]]){
+                right--;
+            }
+
+            if(vowels[c[left]] && vowels[c[right]]){
+                char temp = c[left];
+                c[left] = c[right];
+                c[right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+
+        return new String(c);
+
+    }
+
+}
