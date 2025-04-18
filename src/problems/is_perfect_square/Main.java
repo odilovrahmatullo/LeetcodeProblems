93b88aa842bfa0ed9489ea9beccdffc88f24c750
odilovrahmatullo <odilovrahmatullo5822@gmail.com>
2025-04-18 11:39:14 +0500

    two pointers

--- src/problems/is_perfect_square/Main.java
+++ src/problems/is_perfect_square/Main.java
@@ -1,4 +1,22 @@
 package problems.is_perfect_square;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(20));
+    }
+
+    public static boolean isPerfectSquare(int num) {
+        long low = 0;
+        long high = num;
+        long result = 0;
+        while(low<=high){
+            long mid = (high+low)/2;
+            long sq = mid*mid;
+            if(sq==num) return true;
+            else if(sq<num) low = mid+1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
 }
--- src/problems/sum_of_square_numbers/Main.java
+++ src/problems/sum_of_square_numbers/Main.java
@@ -1,4 +1,48 @@
 package problems.sum_of_square_numbers;
 
 public class Main {
+    public static void main(String[] args) {
+       /* System.out.println("74 = " + judgeSquareSum(74));
+        System.out.println("4 = " + judgeSquareSum(4));
+        System.out.println("5 = " + judgeSquareSum(5));
+        System.out.println("37 = " + judgeSquareSum(37));*/
+       // System.out.println("2147483600 = " + getAbsoluteNumber(2147483600));
+        System.out.println("2147483600 = " + judgeSquareSum(2147483600));
+/*        System.out.println("3 = " + judgeSquareSum(3));*/
+
+
+    }
+
+    public static boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+        while (right >= left) {
+            long sqSum = 1L*right * right + 1L*left * left;
+            if (sqSum == c) return true;
+            else if (sqSum > c) right--;
+            else left++;
+        }
+        return false;
+    }
+
+
+/*    private static int getAbsoluteNumber(int c) {
+        int low = 0;
+        int high = c;
+        int result = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long square = 1L* mid * mid;
+            if (square == c) {
+                result = mid;
+                break;
+            } else if (square < c) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }*/
 }
