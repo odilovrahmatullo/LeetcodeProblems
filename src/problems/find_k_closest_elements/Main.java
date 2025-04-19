781893cecd5753ed1c25a781e1f414ea281043c9
odilovrahmatullo <odilovrahmatullo5822@gmail.com>
2025-04-19 17:46:10 +0500

    Copied find_k_closest_elements from master branch

--- src/problems/find_k_closest_elements/Main.java
+++ src/problems/find_k_closest_elements/Main.java
@@ -1,4 +1,27 @@
 package problems.find_k_closest_elements;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 4, 5};
+        System.out.println(findClosestElements(arr, 4, (-1)));
+    }
+
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        int first = 0;
+        int end = arr.length - 1;
+        while (end - first >= k) {
+            if (Math.abs(arr[first] - x) > Math.abs(arr[end] - x)) first++;
+            else end--;
+        }
+        for (int i = first; i <= end; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
 }
